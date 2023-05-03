7f7e01d8349aa70c167a7eaea7765277177093d5
titus <tkivite@gmail.com>
2023-05-03 11:05:33 +0300

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def seed_admin_user
+    
+    User.create(
+    surname: 'admin',
+    othernames: 'system user',
+    email: 'admin@quikk.com',
+    mobile: '254700000000',
+    password: 'Admin321',
+    status: 1      
+    )
+      
+end
+
+def seed_account
+    user = User.first
+    Account.create(
+    account_number: 'Holding_0001',
+    user_id: user.id,
+    currency: 'KES',
+    balance: 1000000000.00  
+    )
+      
+end
+
+seed_admin_user
+seed_account
