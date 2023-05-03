e0c6c0a62bd98e0946aacac056cf0c0c47dbdf31
Nkuria <nkuria01@gmail.com>
2023-05-03 15:05:51 +0300

    Worked on statemant request and users specs

--- spec/mailers/transaction_spec.rb
+++ spec/mailers/transaction_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe TransactionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe TransactionMailer, type: :mailer do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
