790f6665ae80068abfba8003ec41d45e422bfc8f
Nkuria <nkuria01@gmail.com>
2023-05-03 12:40:04 +0300

    Added sidekiq config in application.rb

--- app/jobs/transaction_statement_job.rb
+++ app/jobs/transaction_statement_job.rb
@@ -2,6 +2,7 @@ class TransactionStatementJob < ApplicationJob
   queue_as :default
 
   def perform(email, data)
-    TransactionMailer.send_csv(email, data)
+    TransactionMailer.send_csv(email, data).deliver_later
   end
+
 end
