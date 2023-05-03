73ee568147f044516755d5c31842ad8b884889e3
Nkuria <nkuria01@gmail.com>
2023-05-03 12:12:47 +0300

    Generated transaction mailer and created method to send email

--- app/services/process_statement_request.rb
+++ app/services/process_statement_request.rb
@@ -13,13 +13,14 @@ class ProcessStatementRequest < ApplicationService
     transactions = Transaction.my_transactions(@user)
     transactions = transactions.where(created_at: @request.start_date.beginning_of_day..@request.end_date.end_of_day)
 
-    CSV.generate(headers: true) do |csv|
-      csv << ['ID', 'Sender', 'Receiver', 'Amount', 'Date']
+    data = CSV.generate(headers: true) do |csv|
+      csv << %w[ID Sender Receiver Amount Date]
       transactions.each do |transaction|
         csv << [transaction.id, transaction.sender.user.email, transaction.recipient.user.email, transaction.amount,
                 transaction.created_at]
       end
     end
-  end
 
+    TransactionMailer.send_csv(@user.email, data).deliver_later
+  end
 end
