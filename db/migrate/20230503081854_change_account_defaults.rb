b5bda2607de9bba49efe83da659c5bf034843da6
hillarioh <okerioh@gmail.com>
2023-05-03 14:25:34 +0300

    update migration

--- db/migrate/20230503081854_change_account_defaults.rb
+++ db/migrate/20230503081854_change_account_defaults.rb
@@ -1,6 +1,6 @@
 class ChangeAccountDefaults < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :accounts, :currency, "KES"
-    change_column_default :accounts, :balance,from: nil, to: 0.0
+    change_column_default :accounts, :currency, from: nil, "KES"
+    change_column_default :accounts, :balance, from: nil, to: 0.0
   end
 end
